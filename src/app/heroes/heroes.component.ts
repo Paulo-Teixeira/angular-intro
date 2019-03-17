30e9b8644ec9babe43e5eef11a7be1cf7c04ac6f
Paulo-Teixeira <paulo.jtfs@:gmail.com>
2019-03-17 12:37:31 +0100

    end of step 5 routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,19 +11,15 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.messageService.add('HeroService: switched hero');
-    this.selectedHero = hero;
-  }
-  getHeroes(): void {
-    this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes);
-  }
-  constructor(private heroService: HeroService, public messageService: MessageService) { }
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
